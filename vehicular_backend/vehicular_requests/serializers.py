f32c3272c836e37956fe42e1d8712d7ac287c3b5
Noe Liceaga <76181731+NoeLiceaga@users.noreply.github.com>
2024-12-01 18:55:10 -0600

    Cambio de archivos

--- vehicular_backend/vehicular_requests/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import VehicularRequest
-
-class VehicularRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicularRequest
-        fields = '__all__'
--- vehicular_backend/vehicular_requests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from vehicular_requests.views import SaveRequestView, LoginView, VehicularRequestsByStatus, SendMessageToBot
-
-urlpatterns = [
-    path('save-request/', SaveRequestView.as_view(), name='save-request'),
-    path('login/', LoginView.as_view(),  name='login'),
-    path('vehicular-requests/', VehicularRequestsByStatus.as_view(), name='open-vehicular-requests'),
-    path('send-message/', SendMessageToBot.as_view(), name='send_message_to_bot')
-]
